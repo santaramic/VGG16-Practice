42bbe4a8c4099b799c87cc97ff44daf81ccea0a3
santaramic <92852096+santaramic@users.noreply.github.com>
2022-02-09 03:43:42 +0900

    Update class_model_12.py

--- class_model_12.py
+++ class_model_12.py
@@ -3,16 +3,16 @@ import tensorflow as tf
 
 
 def Model(input_shape=(256, 256, 3)):
-    h = inputs = tf.keras.Input(input_shape)    # 이 코드의 출력 shape은 어떻게 되는가?
+    h = inputs = tf.keras.Input(input_shape)   
 
     h = tf.keras.layers.ZeroPadding2D((3, 3))(h)
     h = tf.keras.layers.Conv2D(filters=64,
                                kernel_size=7,
                                strides=2,
                                padding="valid",
-                               use_bias=False)(h)   # weight의 개수는 몇개인가? 또한 bias가 포함되어있을 떄는 어떻게 되는가?
-    h = tf.keras.layers.BatchNormalization()(h) # 이 기능에 대해 간략히 설명하세요.
-    h = tf.keras.layers.ReLU()(h)   # 이 기능에 대해 간략히 설명하세요.
+                               use_bias=False)(h)   
+    h = tf.keras.layers.BatchNormalization()(h) 
+    h = tf.keras.layers.ReLU()(h)   
 
     h = tf.keras.layers.Conv2D(filters=128,
                                kernel_size=3,
@@ -22,7 +22,7 @@ def Model(input_shape=(256, 256, 3)):
     h = tf.keras.layers.BatchNormalization()(h) 
     h = tf.keras.layers.ReLU()(h)
 
-    h = tf.keras.layers.MaxPooling2D(pool_size=(2,2), strides=2)(h) # 이 기능에 대해 간략히 설명하세요.
+    h = tf.keras.layers.MaxPooling2D(pool_size=(2,2), strides=2)(h) 
 
     h = tf.keras.layers.Conv2D(filters=256,
                                kernel_size=3,
